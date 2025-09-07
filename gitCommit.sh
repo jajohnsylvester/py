30ba3408b98a17c2446438fe819e9129eb63f60f
John Sylvester <103321022+jajohnsylvester@users.noreply.github.com>
2025-09-07 21:53:43 +0530

    Update gitCommit.sh

--- gitCommit.sh
+++ gitCommit.sh
@@ -3,5 +3,10 @@ git config --global user.name "John Sylvester"
 git add .
 git commit -m "Changed $(date +'%Y-%m-%d %H:%M:%S')"
 
-git push -u https://jajohnsylvester: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@github.com/jajohnsylvester/jupiternotebook.git main
+# The Base64 encoded string
+encoded_string="Z2hwXzJDZVc4YWdaMkNmRWdZN1dEUmZEbHpKVWNWQVVtcTJYQ0JLaA==="
+
+# Decode the string and store it in a variable
+decoded_variable=$(echo "$encoded_string" | base64 --decode)
+git push -u https://jajohnsylvester:$decoded_variable@github.com/jajohnsylvester/jupiternotebook.git main
 
