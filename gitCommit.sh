02de33849d2f8b64b4cf384ecb5bcf18c0a53d7f
John Sylvester <103321022+jajohnsylvester@users.noreply.github.com>
2025-09-07 21:26:45 +0530

    Update gitCommit.sh

--- gitCommit.sh
+++ gitCommit.sh
@@ -2,4 +2,5 @@ git config --global user.email "jajohnsylvester@gmail.com"
 git config --global user.name "John Sylvester"
 git add .
 git commit -m "Changed $(date +'%Y-%m-%d %H:%M:%S')"
-git push -u https://jajohnsylvester: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@github.com/jajohnsylvester/jupiternotebook.git main
+
+git push -u https://jajohnsylvester:git push -u https://jajohnsylvester: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@github.com/jajohnsylvester/jupiternotebook.git main@github.com/jajohnsylvester/jupiternotebook.git main
