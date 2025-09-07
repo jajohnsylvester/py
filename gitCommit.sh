938781add993701ac7c0b9974be429d058ec4090
John Sylvester <103321022+jajohnsylvester@users.noreply.github.com>
2025-09-07 20:37:08 +0530

    Add files via upload

---
+++ gitCommit.sh
@@ -0,0 +1,5 @@
+git config --global user.email "jajohnsylvester@gmail.com"
+git config --global user.name "John Sylvester"
+git add .
+git commit -m "Changed $(date +'%Y-%m-%d %H:%M:%S')"
+git push -u https://jajohnsylvester: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@github.com/jajohnsylvester/jupiternotebook.git main
